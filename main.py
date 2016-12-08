ef3a4b5219a92ff59067a658af33d241f6b8fc6c
yetoo-online <yetoohappy@gmail.com>
2016-12-08 16:56:17 +0000

    made the file be able to read correctly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import urllib2
-response =urllib2.urlopen('http://www.lgb.org/travelers/flight_status.asp')
-html = repsponse.read()
+response = urllib2.urlopen('http://www.lgb.org/travelers/flight_status.asp')
+html = response.read()
 #print html
+html = html.split("\n")
+for line in html:
+	print line
