8f52ac982582cdca2f7f3a59f1cfc083ea996450
yetoo-desktop <yetoohappy@gmail.com>
2016-12-09 16:58:10 -0800

    fixed some sutff and successfully sliced, next need to parse into a temp or something

--- main.py
+++ main.py
@@ -8,16 +8,46 @@ html = html.split("\n")
 #for line in html:
 	#print line
 	#if (line)
-pattern = "<tbody>"
-for line in html:
-     #for c in line:
-	#print line
-	if "<tbody>" in line:
-		#print "found"
-		match = re.search(pattern, line)
-		s = match.start()
-		print s
-		break
+#for the gui, the table of values will show
+#also, ask teacher for verification if this is going in the right direction
+def arrivals():
+#this little bit is only for the arrivals, the departures will come later
+#I also don't even know how this is managing to work since there are <td> tags before the ones we need>
+#This will break if the user sets a number too high as, the script will think that the data is coming from departures, so there needs to be a set stopping point. The only problem is that I'm having a hard time counting each on that happens, just to note, on the link above, the site shows 17 entries but the html file I downloaded separate from the html downloaded in the code has more than 17. Someone will eventually need to fix that by counting how much there are. WAIT I HAVE FOUND THE CLASS WHICH CONTAINS THIS BUT IT ENDS AT 19 IN CASE ANYONE WANTS TO KNOW
+#also, as of now this is the console version, the final version will be gui	
+	i = 0
+	k = 0
+	listamount = 7 #this variable will be changed by the user and will have a global varient just incase the user wants to see both the arrivals and departures by this interval. The maximum possibility of this variable for a unique result is 19. The default could be 7 but it most likely will change.
+	pattern = "<td>"
+	pattern2 = "<div class=\"view view-departures"
+	for line in html:
+	     #for c in line:
+		#print line
+		#this is here to stop any possibility of going over to the next dataset
+		if pattern2 in line:
+					print "huh"
+					break		
+		if pattern in line:
+			#is just finding location
+			#get the ontime part
+			#the match and stuff isn't needed, it's kept for now just in case it actually is needed			
+			match = re.search(pattern, line)
+			s = match.start()
+			linenospace = line.split("</td>", 1)[0]
+			linenospace2 = linenospace.split("<td>", 1)[-1]	
+			print linenospace2		
+			i += 1
+			#as of now it's modulo 4 but it should become five for a tag that it's not reading but soon will
+			if i % 4 == 0:			#this is for debugging
+				print "-----",i/4,"----"	#also this is part of the comment above	
+			if i % 4 == 0 and i == listamount * 4: 
+				#print "end" this won't work because this doesn't mean it won't stop iterating
+				break
+				#pass
+				
+	
+		
+arrivals()		
 	
 #tagb = 0
 #tagba = 0
