9dc5c52abac15db366150d523e7d3c692b34773a
yetoo-desktop <yetoohappy@gmail.com>
2016-12-09 20:31:56 -0800

    It's still kind of broken, but I have an understanding on what's going on, the amount changes each time so the existing code is assuming that there is a constant value, but there isn't, other than atleast 4 for each batch of the tags, butessentially there needs to be a counter that gets set to the amount of the arrivals and then use that value to help calculate with the departures

--- main.py
+++ main.py
@@ -38,14 +38,16 @@ html = html.split("\n")
 #yes a better way would to be splitting the file up into two parts so you don't have to wait for the program to trip over itself, but I really don't want to at the moment, if someone else does plase implement it 
 #for the gui, the table of values will show
 #also, ask teacher for verification if this is going in the right direction
+#make recommended to view 7 at once as the actual page changes count, wait crap, it doesn't matter just make it all some large number, like 35 so that it gets a list of all planes, but the user can still decide 
 def arrivals():
+	print "Arrivals\n"
 #this little bit is only for the arrivals, the departures will come later
 #I also don't even know how this is managing to work since there are <td> tags before the ones we need>
 #This will break if the user sets a number too high as, the script will think that the data is coming from departures, so there needs to be a set stopping point. The only problem is that I'm having a hard time counting each on that happens, just to note, on the link above, the site shows 17 entries but the html file I downloaded separate from the html downloaded in the code has more than 17. Someone will eventually need to fix that by counting how much there are. WAIT I HAVE FOUND THE CLASS WHICH CONTAINS THIS BUT IT ENDS AT 19 IN CASE ANYONE WANTS TO KNOW
 #also, as of now this is the console version, the final version will be gui	
 	i = 0
 	k = 0
-	listamount = 7 #this variable will be changed by the user and will have a global varient just incase the user wants to see both the arrivals and departures by this interval. The maximum possibility of this variable for a unique result is 19. The default could be 7 but it most likely will change. When global variable implemented, must create if statements to detect a switch that occurs when a button is pressed to use gloabl instead of local
+	listamount = 19 #this variable will be changed by the user and will have a global varient just incase the user wants to see both the arrivals and departures by this interval. The maximum possibility of this variable for a unique result is 19 NO WAIT IT COULD BE 12? CRAP IT CHANGES!. The default could be 7 but it most likely will change. When global variable implemented, must create if statements to detect a switch that occurs when a button is pressed to use gloabl instead of local
 	pattern = "<td>"
 	pattern2 = "<div class=\"view view-departures"
 	for line in html:
@@ -53,8 +55,9 @@ def arrivals():
 		#print line
 		#this is here to stop any possibility of going over to the next dataset
 		if pattern2 in line:
-					print "huh"
-					break		
+			print "Reached the end of section!"					
+			break		
+		
 		if pattern in line:
 			#is just finding location
 			#get the ontime part
@@ -66,35 +69,48 @@ def arrivals():
 			print linenospace2		
 			i += 1
 			#as of now it's modulo 4 but it should become five for a tag that it's not reading but soon will
-			if i % 4 == 0:			#this is for debugging
+			if i % 4 == 0:			#this is for debugging			
 				print "-----",i/4,"-----"	#also this is part of the comment above	
 			if i % 4 == 0 and i == listamount * 4: 
-				#print "end" this won't work because this doesn't mean it won't stop iterating
+				#print "end" this won't work because this doesn't mean it won't stop iterating			
+				print "Reached the end of the user's selection. of arrivals."			
 				break
-				#pass
-def departures():				
+								
+			#fix	
+def departures():
+	print "Departures:\n"				
+	#i = 76 #doesn't matter if you set the i, it starts at the same place, remember we are sorting lines here, we are merely manipulating the mechanisim to sort through lines.
 	i = 0
 	k = 0
-	verified = 0 #verified set to 1 means depature head has been met and 0 means that it has no
-	listamount = 7
-	start = "<div class=\"view view-departures"
+	verify = 0
+	listamount = 19
+	#start = "view-id-departures"
 	pattern = "<td>"
 	for line in html:
-		if verified != 1:
-			pass
-		else:
-			if pattern in line:
-				linenospace = line.split("</td>", 1)[0]
-				linenospace2 = linenospace.split("<td>", 1)[-1]
+	#for i in range (76, len(html))	
+		if pattern in line:			
+			linenospace = line.split("</td>", 1)[0]
+			linenospace2 = linenospace.split("<td>", 1)[-1]
+			if i > 75:						
 				print linenospace2
-				i += 1
-				if i % 4 == 0:
-					print "-----",i/4,"d-----" # the d is for departure to duifferentiate from the detatched present which is is the arrivals (failed attempt at aliteration) 
-				if i % 4 == 0 and i == listamount * 4:
-					break
+			i += 1
+			if i % 4 == 0 and i > 76: #19 * 4
+				#a = i - 76				
+				print "-----",(i/4)-19,"d-----" # the d is for departure to differentiate from the detatched present which is is the arrivals (failed attempt at aliteration)
+			#this is the proprietary, this is what we are manipulating, the if statement, we are chaning the amount of water that goes into the stream, the if statement is the log. Just changing the rate of the water will change it, i, it's the ultimate exit that changes it.  
+			#if i % 4 == 0 and i == (listamount * 4)+76: #19 * 4
+			if i % 4 == 0 and i == listamount * 4:			
+				print "Reached the end of the user's selection of departures."		
+				break
+			
+				
 
 arrivals()
-departures()		
+print "\n"
+departures()	
 	
-
+#plane
+#city
+#time
+#gate
 
